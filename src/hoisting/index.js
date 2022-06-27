cb9513779f38167ab5bd5e8a89c2a52e3f8033a7
Joao Garduño <joao.garduno@gmail.com>
2022-06-27 13:55:16 -0500

    Estructura 8: Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,21 @@
+console.log(nameOfDog);
+var nameOfDog = 'Snoopy';
+console.log(nameOfDog);
+
+// --------------------------------
+// Vamos a probar con una funcion
+
+// Mandamos a llamar antes la funcion
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perro es ${namePerro}`);
+}
+
+// var namePerro = 'Snoopy';
+
+// Que pasaria si ejecutamos el siguiente codigo sin nada, es decir si le quitamos el siguiente codigo (var namePerro = 'snoopy')
+// var namePerro = 'Snoopy';
+
+// Pero si agregamos o definimos una varible si se ejecuta el codigo
+var namePerro;
